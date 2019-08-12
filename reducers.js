c7f68d7e8300ddd092bc5922d7ebeacf0ab43b8d
alexodorico <alex.odorico@morleynet.com>
2019-08-12 00:03:41 -0400

    Started defining initial state and action constants

---
+++ reducers.js
@@ -0,0 +1,18 @@
+const initialState = {
+  loggedIn: false,
+  username: undefined,
+  joinedGame: false,
+  waitingForPlayer: false,
+  gameStarted: false,
+  gameId: undefined,
+  players: {
+    white: undefined,
+    black: undefined
+  },
+  turn: undefined,
+  board: undefined,
+  check: false,
+  checkmate: false,
+  gameEnded: false,
+  winner: false
+};
